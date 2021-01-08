8d952a24416646b324dfbbdd1084b33901c05a01
Jarom Plewe <jplewe23@gmail.com>
2021-01-08 01:27:57 -0700

    refactored code- only contains one loop and one counter

--- index.js
+++ index.js
@@ -1,47 +1,101 @@
 let textInput = document.getElementById('textInput');
 let submitBtn = document.getElementById('submitBtn');
 
-
 let findBracketMatch = (codeString) => {
+    
+    let matchConfirmation = false;
+    let bracketCounter = 0;
+    let codeStringArr = codeString.split('')
 
-    let matchConfirmation = Boolean;
-    let openBrackets = [];
-    let closedBrackets = [];
-
-    codeString.split('').forEach(
-        function findOpenBrackets(item, index) {
-            if (item === '{') {
-                openBrackets.push(index);
-            }
-            if (item === '}') {
-                closedBrackets.push(index);
-            }
-
+    for (let i = 0; i < codeStringArr.length; i++) {
+        if (codeStringArr[i] == '{') {
+            bracketCounter += 1;
         }
-    );
+        if (codeStringArr[i] == '}') {
+            bracketCounter -= 1;
+        }
+        if (bracketCounter < 0) {
+            break;
+        }
+    }
 
-    if (openBrackets.length !== closedBrackets.length) {
-        matchConfirmation = false;
-    } else if (!codeString.includes('{') && !codeString.includes('}')) {
+    if (bracketCounter === 0) {
         matchConfirmation = true;
-    } else {
-        openBrackets.forEach(
-            function findClosedBrackets(item, index) {
-                if (codeString.includes('}', item)) {
-                    matchConfirmation = true;
-                } else {
-                    matchConfirmation = false;
-                }
-            }
-        );
-    };
+    }
 
     console.log("Match Confirmed? ", matchConfirmation);
 };
-
 
 submitBtn.onclick = (e) => {
     e.preventDefault();
     let codeString = textInput.value;
     findBracketMatch(codeString);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let textInput = document.getElementById('textInput');
+// let submitBtn = document.getElementById('submitBtn');
+
+
+// let findBracketMatch = (codeString) => {
+
+//     let matchConfirmation = false;
+//     let openBrackets = [];
+//     let closedBrackets = [];
+
+//     codeString.split('').forEach(
+//         function findOpenBrackets(item, index) {
+//             if (item === '{') {
+//                 openBrackets.push(index);
+//             }
+//             if (item === '}') {
+//                 closedBrackets.push(index);
+//             }
+
+//         }
+//     );
+
+//     if (openBrackets.length !== closedBrackets.length) {
+//         return;
+//     } else if (!codeString.includes('{') && !codeString.includes('}')) {
+//         matchConfirmation = true;
+//     } else {
+//         openBrackets.forEach(
+//             function findClosedBrackets(item, index) {
+//                 if (codeString.includes('}', item)) {
+//                     matchConfirmation = true;
+//                 } else {
+//                     matchConfirmation = false;
+//                 }
+//             }
+//         );
+//     };
+
+//     console.log("Match Confirmed? ", matchConfirmation);
+// };
+
+
+// submitBtn.onclick = (e) => {
+//     e.preventDefault();
+//     let codeString = textInput.value;
+//     findBracketMatch(codeString);
+// }
